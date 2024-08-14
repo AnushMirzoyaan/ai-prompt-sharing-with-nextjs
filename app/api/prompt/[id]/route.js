b12ed97f210b2c94e6f8400164cf79f409346613
AnushMirzoyaan <anush.mirzoyan1999@gmail.com>
2024-08-14 19:49:23 +0400

    adding routes

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,54 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    const promt = await Prompt.findById(params.id).populate("creator");
+
+    if (!promt) {
+      return new Response("Prompt not found.", { status: 404 });
+    }
+
+    return new Response(JSON.stringify(promt), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new Response("Failed  to fetch prompt", { status: 500 });
+  }
+};
+
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.json();
+
+  try {
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if (!existingPrompt) {
+      return new Response("Prompt not found.", { status: 404 });
+    }
+
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed  to update prompt", { status: 500 });
+  }
+};
+
+export const DELETE = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    await Prompt.findByIdAndDelete(params.id);
+
+    return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+    return new Response("Failed  to delete prompt", { status: 500 });
+  }
+};
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,11 +1,12 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
 
@@ -21,7 +22,9 @@ const MyProfile = () => {
     }
   }, []);
 
-  const handleEdit = () => {};
+  const handleEdit = () => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
   const handleDelete = () => {};
 
