61e69ea0c98040a4c564481ed2dcc26ec2443ca6
AnushMirzoyaan <anush.mirzoyan1999@gmail.com>
2024-08-30 19:40:39 +0400

    fixing logic issue

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,6 @@
 import PromptCard from "./PromptCard";
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
-  console.log(handleEdit);
   return (
     <section className="w-full">
       <h1 className="head_text text-left">
@@ -14,8 +13,8 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
           <PromptCard
             key={post._id}
             post={post}
-            handleTagClick={handleEdit && handleEdit(post)}
-            handleDelete={handleDelete && handleDelete(post)}
+            handleEdit={handleEdit ? () => handleEdit(post) : undefined}
+            handleDelete={handleDelete ? () => handleDelete(post) : undefined}
           />
         ))}
       </div>
